fe43c72523442334320489a46a5572c071f8b57c
justinkimsgithub <justinkimsemail@gmail.com>
2018-11-09 17:41:57 -0800

    search endpoint

---
+++ ScheduleMaker/src/services/Search.java
@@ -0,0 +1,52 @@
+package services;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import database.Firebase;
+
+/**
+ * Servlet implementation class Search
+ */
+@WebServlet("/search")
+public class Search extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Search() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+				String email = request.getParameter("email");
+				String query = request.getParameter("query");
+				String json;
+				System.out.println("QUERY: " + query);
+				json= new Gson().toJson(Firebase.getUsers(email, query));
+
+			    response.setContentType("application/json");
+			    response.setCharacterEncoding("UTF-8");
+			    response.getWriter().write(json);
+			    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
