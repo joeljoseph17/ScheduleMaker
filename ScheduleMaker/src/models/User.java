7bdbfdcd0bc74491d16ff2c7cb41d80ea653f832
justinkimsgithub <justinkimsemail@gmail.com>
2018-11-09 16:54:58 -0800

    partial search unfctionality

--- ScheduleMaker/src/models/User.java
+++ ScheduleMaker/src/models/User.java
@@ -6,13 +6,13 @@ import api.Schedule;
 public class User {
 	
 	List<String> friends;
-	String name;
+	List<String> name;
 	List<String> savedSchedules;
 	
 	public List<String> getFriends() {
 		return friends;
 	}
-	public String getName() {
+	public List<String> getName() {
 		return name;
 	}
 	public List<String> getSavedSchedules() {
